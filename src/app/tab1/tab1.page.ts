fdb80c1fc6fbae2f0ae0ffe2f0d74c936b59a731
Sarah Oliveira <soliveirarm@outlook.com>
2024-03-08 11:15:45 -0300

    add new task input

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  tasks: any[] = [];
+  taskTitle: string = "";
 
   constructor() {}
+
+  addTask() {
+    if (this.taskTitle.length > 0) {
+      let task = {title:this.taskTitle}
+      this.tasks.push(task)
+      this.taskTitle = ""
+    }
+  }
 
 }
